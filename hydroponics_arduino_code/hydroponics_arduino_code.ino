91e2ae6eb295f51efc3f91e1d873c5d75df780ab
jannepetter <peterdev@gmail.com>
2021-04-28 21:44:56 +0300

    yellow light fix

--- hydroponics_arduino_code/hydroponics_arduino_code.ino
+++ hydroponics_arduino_code/hydroponics_arduino_code.ino
@@ -1,13 +1,14 @@
+
 int duty_cycle = 200; // Between 0 and 255, controls the motor speed
 int up_threshold = 500;
 int down_threshold_red = 400;
-int down_threshold_yellow = 600;
+int down_threshold_yellow = 520;
 int val_up;
 int val_down;
-unsigned long time_now = 0;
-unsigned long fill_moment = 0;
+
 String down = "down";
 String up = "up";
+
 int RED_LED = 8;
 int YELLOW_LED = 7;
 int MOTORPIN = 9;
@@ -17,7 +18,7 @@ int UPSIGNALPIN = A0;
 int DOWNSIGNALPIN = A1;
 int loopcount = 0;
 int maxloopcount = 56;
-int interval = 90000;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,46 +33,39 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(UPSENSOR_OUT, HIGH);
-  delay(10);
-  val_up = analogRead(UPSIGNALPIN);
-  Serial.println(up + val_up);
-  time_now = millis(); // time in milliseconds from the beginning of the program
-  if (val_up < up_threshold and (time_now - fill_moment) > interval) {
-    analogWrite(MOTORPIN, duty_cycle);
-    while (val_up < up_threshold and loopcount < maxloopcount){
-      delay(250);
-      val_up = analogRead(UPSIGNALPIN);
-      Serial.println(up + val_up);
-      loopcount += 1;
-    }
-    loopcount = 0;
-    analogWrite(MOTORPIN, 0);
-    time_now = millis();
-    fill_moment = time_now;
-  }
-  digitalWrite(UPSENSOR_OUT, LOW);
-  
-  if (fill_moment > time_now) {
-    // millis() resets in about each 50 days
-    // this will reset the time of the last fill moment when the reset of millis() occurs
-    fill_moment = time_now;
-  }
-
   digitalWrite(DOWNSENSOR_OUT, HIGH);
   delay(10);
+  val_up = analogRead(UPSIGNALPIN);
   val_down = analogRead(DOWNSIGNALPIN);
   digitalWrite(DOWNSENSOR_OUT, LOW);
+  digitalWrite(UPSENSOR_OUT, LOW);
+
+  // for tuning
+  Serial.println(up + val_up);
   Serial.println(down + val_down);
-  if (val_down < down_threshold_red) {
-    digitalWrite(RED_LED, HIGH);
-    delay(1000);
-    digitalWrite(RED_LED, LOW);
-  } else if (val_down > down_threshold_yellow) {
-    digitalWrite(YELLOW_LED, HIGH);
-    delay(1000);
-    digitalWrite(YELLOW_LED, LOW);
+  Serial.println(loopcount);
+  // 40 loops rest period (also seconds as one loop last 1s)
+  if (loopcount > 40) {
+    analogWrite(MOTORPIN, duty_cycle);
   }
-  delay(980);
+  // cut motor power and reset count when sensor says so or reach maxloopcount as backup
+  if (val_up > up_threshold || loopcount >= maxloopcount){
+    loopcount=0;
+    analogWrite(MOTORPIN, 0);
+  }
+
+   if (val_down < down_threshold_red) {
+    digitalWrite(RED_LED, HIGH);
+  }
+  if (val_down > down_threshold_yellow) {
+    digitalWrite(YELLOW_LED, HIGH);
+  }
+
+  delay(480);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  loopcount++;
+  delay(500);
 }
+
